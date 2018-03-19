d04e8e5a12e17dd3d80683039756bc2b6019a195
Graham Colgate <graham@gramtech.co.uk>
2018-03-19 12:00:30 +0000

    commenting out the chsh command. Added more details around what it is

--- homebrew/brewfile.sh
+++ homebrew/brewfile.sh
@@ -265,15 +265,17 @@ brew install zsh
 # Properly setting up bash
 ##########################################
 # Homebrew's install of bash sets it up in your $PATH as with most brew installs, however 
-# it appears does not change your the users shell to use the new bash install
-# This can cause errors, for example when adding bash-completion to your .bash_pofile
-# The following sets up bash properly with your machine and your user shell 
+# it does not change your user shell to use the new bash install
+# This can cause errors, for example when adding bash-completion@2 to your .bash_pofile
+# The following sets up brew bash your user shell 
 # This does require sudo and your user password
+# This change can be descrutive so the below is commented out and you can add in as required or do not use completion@2:
+# see here for details - https://github.com/Homebrew/homebrew-core/issues/25370
 
-# Add the new shell to the list of allowed shells
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-# Change to the new shell
-chsh -s /usr/local/bin/bash 
+# # Add the new shell to the list of allowed shells
+# sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# # Change to the new shell
+# chsh -s /usr/local/bin/bash 
 
 
 ##########################################
