66b1a16518621243a6ed2ad97d5eae351d96f229
Sebastian Rousseau <sebastian.rousseau@gmail.com>
2022-09-21 14:08:04 +0100

    fix(dotfiles): minor updates

--- src/dotfiles/aliases/tmux/tmux.plugin.sh
+++ src/dotfiles/aliases/tmux/tmux.plugin.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 # 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.450)
 
-alias tm='tmux'                   # tm: Start tmux.
-alias tmks='tmux kill-session -a' # tmks: Kill all tmux sessions.
-alias tml='tmux list-sessions'    # tml: List tmux sessions.
-alias tms='tmux new-session -s'   # tms: Start a new tmux session.
+alias tm='tmux'                     # tm: Start tmux.
+alias tma="tmux attach-session"     # tma: Attach to a tmux session.
+alias tmat="tmux attach-session -t" # tmat: Attach to a tmux session with name.
+alias tmks='tmux kill-session -a'   # tmks: Kill all tmux sessions.
+alias tml='tmux list-sessions'      # tml: List tmux sessions.
+alias tmn="tmux new-session"        # tmn: Start a new tmux session.
+alias tmns="tmux new -s"            # tmns: Start a new tmux session with name.
+alias tms='tmux new-session -s'     # tms: Start a new tmux session.
