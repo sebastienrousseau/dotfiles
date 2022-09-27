bb25a2dc95d230bfc278c0c6c425a2e4ec88df14
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 18:50:01 +0100

    fix(path): updated ant and java paths

--- shell/paths/java/java.path.sh
+++ shell/paths/java/java.path.sh
@@ -6,7 +6,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
   export PATH=/opt/homebrew/opt/openjdk/bin:"$PATH" # Java binaries
-  JAVA_HOME="$(brew --prefix)/Cellar/openjdk/$(java -version 2>&1 | head -n 1 | cut -d'"' -f2)/libexec/openjdk.jdk/Contents/Home"
+  JAVA_HOME="/opt/homebrew/Cellar/openjdk/$(java -version 2>&1 | head -n 1 | cut -d'"' -f2)/libexec/openjdk.jdk/Contents/Home"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   JAVA_HOME="/usr/lib/jvm/java-11-openjdk-arm64/"
 fi
