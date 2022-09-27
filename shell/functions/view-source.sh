d0216e3e4f2f3ad48f5cb6b4a877deb9958e3127
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 19:25:28 +0100

    fix(fn): ShellCheck only supports sh/bash/dash/ksh scripts.

--- shell/functions/view-source.sh
+++ shell/functions/view-source.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 # view-source: Function to view the source of a website.
 view-source () { /usr/bin/curl -L -k -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0' $@ ; }
