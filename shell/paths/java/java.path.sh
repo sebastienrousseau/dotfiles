e307aba2505f5fbf8b8715cce2e8e22734fd0950
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 17:56:17 +0100

    fix(java): changed path

--- shell/paths/java/java.path.sh
+++ shell/paths/java/java.path.sh
@@ -8,7 +8,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   export PATH=/opt/homebrew/opt/openjdk/bin:"$PATH" # Java binaries
   JAVA_HOME="$(brew --prefix)/Cellar/openjdk/18.0.2.1_1/libexec"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  JAVA_HOME="/usr/lib/jvm/java-11-openjdk-arm64/bin/java"
+  JAVA_HOME="/usr/lib/jvm/java-11-openjdk-arm64/"
 fi
 export JAVA_HOME
 export JRE_HOME="${JAVA_HOME}"/jre
