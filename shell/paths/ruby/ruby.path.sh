bc6c7d2c379068c0df255557d438143c065d8936
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 08:12:30 +0100

    fix(path): updated ruby path

--- shell/paths/ruby/ruby.path.sh
+++ shell/paths/ruby/ruby.path.sh
@@ -4,9 +4,9 @@
 ## 🆁🆄🅱🆈 🅷🅾🅼🅴
 if [ -z "$RUBY_HOME" ]; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    RUBY_HOME=$(which ruby)
+    RUBY_HOME="$(brew --prefix)/opt/ruby/bin"
   elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    RUBY_HOME=" /usr/lib/ruby/2.6.0 "
+    RUBY_HOME="/usr/lib/ruby/2.6.0/"
   fi
   export RUBY_HOME
   export PATH="$RUBY_HOME:$PATH"
@@ -18,8 +18,8 @@ if [ -z "$GEM_HOME" ]; then
     GEM_HOME="$(brew --prefix)/Cellar/ruby/3.1.2_1/lib/ruby/gems/3.1.0/"
     GEM_PATH="$(brew --prefix)/Cellar/ruby/3.1.2/lib/ruby/gems/3.1.0/"
   elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    GEM_HOME=" /usr/lib/ruby/2.6.0 "
-    GEM_PATH=" /usr/lib/ruby/2.6.0 "
+    GEM_HOME="/usr/lib/ruby/2.6.0/"
+    GEM_PATH="/usr/lib/ruby/2.6.0/"
   fi
   export GEM_HOME
   export GEM_PATH
