4ed7030420653b81ed1b62eaf06f0248a305b38d
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 18:04:11 +0100

    fix(mvn): updated maven home

---
+++ shell/paths/maven/maven.path.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.450) - MAVEN Path configuration.
+
+## 🅼🅰🆅🅴🅽 🅷🅾🅼🅴
+if [ -z "$MAVEN_HOME" ]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    MAVEN_HOME="$(brew --prefix)/Cellar/maven/$(mvn -v)/libexec"
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    MAVEN_HOME="/usr/share/maven/"
+  fi
+  M2_HOME="$MAVEN_HOME"
+  export MAVEN_HOME M2_HOME
+  export PATH="$MAVEN_HOME:$PATH"
+fi
