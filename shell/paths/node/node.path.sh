f75159e60ddaac1d50151316eb9bdfed05e6d956
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-27 17:50:22 +0100

    fix(node): updated path

--- shell/paths/node/node.path.sh
+++ shell/paths/node/node.path.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.450) - PNPM Path configuration.
+# 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.450) - NODE Path configuration.
 
 ## 🅽🅾🅳🅴 🅿🅰🆃🅷
 if [ -z "$NODE_PATH" ]; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
     NODE_PATH=$(which node)
   elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    NODE_PATH="$HOME"/.local/share/pnpm
+    NODE_PATH=$HOME/.nvm/versions/node/$(node -v)/bin/node
   fi
   export NODE_PATH
   export PATH="$NODE_PATH:$PATH"
