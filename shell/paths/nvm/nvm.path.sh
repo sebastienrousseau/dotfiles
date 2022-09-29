f5cce0a5bbd45a9387cb510acfd529081896a11b
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-09-29 22:36:24 +0100

    fix(aliases): changed file names

--- shell/paths/nvm/nvm.path.sh
+++ shell/paths/nvm/nvm.path.sh
@@ -2,21 +2,21 @@
 # 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.451) - NVM Path configuration.
 
 ## 🅽🆅🅼 🅿🅰🆃🅷
-if [ -z "$NVM_DIR" ]; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    NVM_DIR="$HOME"/.nvm
+if [[ -z "${NVM_DIR}" ]]; then
+  if [[ "${OSTYPE}" == "darwin"* ]]; then
+    NVM_DIR="${HOME}"/.nvm
     # shellcheck source=/dev/null
-    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+    [[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
     # shellcheck source=/dev/null
-    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+    [[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    NVM_DIR="$HOME"/.nvm
+  elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
+    NVM_DIR="${HOME}"/.nvm
     # shellcheck source=/dev/null
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+    [[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh" # This loads nvm
     # shellcheck source=/dev/null
-    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+    [[ -s "${NVM_DIR}/bash_completion" ]] && . "${NVM_DIR}/bash_completion" # This loads nvm bash_completion
   fi
   export NVM_DIR
-  export PATH="$NVM_DIR:$PATH"
+  export PATH="${NVM_DIR}:${PATH}"
 fi
--- shell/paths/ruby/ruby.path.sh
+++ shell/paths/ruby/ruby.path.sh
@@ -2,27 +2,27 @@
 # 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.451) - PNPM Path configuration.
 
 ## 🆁🆄🅱🆈 🅷🅾🅼🅴
-if [ -z "$RUBY_HOME" ]; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ -z "${RUBY_HOME}" ]]; then
+  if [[ "${OSTYPE}" == "darwin"* ]]; then
     RUBY_HOME="$(brew --prefix)/opt/ruby/bin"
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
     RUBY_HOME="/usr/lib/ruby/2.6.0/"
   fi
   export RUBY_HOME
-  export PATH="$RUBY_HOME:$PATH"
+  export PATH="${RUBY_HOME}:${PATH}"
 fi
 
 ## 🅶🅴🅼 🅷🅾🅼🅴
-if [ -z "$GEM_HOME" ]; then
-  if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ -z "${GEM_HOME}" ]]; then
+  if [[ "${OSTYPE}" == "darwin"* ]]; then
     GEM_HOME="$(brew --prefix)/Cellar/ruby/3.1.2_1/lib/ruby/gems/3.1.0/"
     GEM_PATH="$(brew --prefix)/Cellar/ruby/3.1.2/lib/ruby/gems/3.1.0/"
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
     GEM_HOME="/usr/lib/ruby/2.6.0/"
     GEM_PATH="/usr/lib/ruby/2.6.0/"
   fi
   export GEM_HOME
   export GEM_PATH
-  export PATH="$GEM_PATH:$PATH"
-  export PATH="$GEM_HOME:$PATH"
+  export PATH="${GEM_PATH}:${PATH}"
+  export PATH="${GEM_HOME}:${PATH}"
 fi
