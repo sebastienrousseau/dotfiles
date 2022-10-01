d7538a752d5aa2b454ddf07264b2ee6273f3880f
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-01 16:33:38 +0100

    fix(editor): Error Prone issue

--- shell/configurations/default/editor.sh
+++ shell/configurations/default/editor.sh
@@ -3,15 +3,14 @@
 # Copyright (c) Sebastien Rousseau 2022. All rights reserved
 # License: MIT
 
- - Editor configuration.
-
-if ! [ "$(command -v vim)" ]; then
+cmd_vim=$(command -v vim)
+if ! [[ -n ${cmd_vim} ]]; then
   export VISUAL="vi"
 else
   export VISUAL="vim"
 fi
 
-export EDITOR=$VISUAL
-export GIT_EDITOR=$VISUAL
-export SVN_EDITOR=$VISUAL
-export SUDO_EDITOR=$VISUAL
+export EDITOR=${VISUAL}
+export GIT_EDITOR=${VISUAL}
+export SVN_EDITOR=${VISUAL}
+export SUDO_EDITOR=${VISUAL}
