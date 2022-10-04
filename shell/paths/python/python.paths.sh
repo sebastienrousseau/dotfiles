d57bcc809a9f77e6e7217b94a1e61110d64654a6
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-04 18:19:15 +0100

    feat(python):added python path

---
+++ shell/paths/python/python.paths.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.452) - https://dotfiles.io
+# Copyright (c) Sebastien Rousseau 2022. All rights reserved
+# License: MIT
+
+## 🅿🆈🆃🅷🅾🅽 🅿🅰🆃🅷
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+  if [[ -d "/opt/homebrew/opt/python/bin" ]]; then
+    PYTHONHOME="/opt/homebrew/opt/python/bin"
+  fi
+elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
+  PYTHONHOME="/usr/lib/python*/"
+fi
+export PYTHONHOME
+export PATH="${PYTHONHOME}:${PATH}"
