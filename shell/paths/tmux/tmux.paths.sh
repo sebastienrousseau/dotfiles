00adce32fba030c8eb4233019d326d84825bed95
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-05 21:32:46 +0100

    fix(tmux): tmux server immediately exits on new session

--- shell/paths/tmux/tmux.paths.sh
+++ shell/paths/tmux/tmux.paths.sh
@@ -7,9 +7,8 @@
 if command -v 'tmux' >/dev/null; then
   TMUX_PATH=$(command -v tmux)
   export TMUX_PATH
-
 fi
 
 if [[ -z "${TMUX}" ]] && [[ ${UID} != 0 ]]; then
-  tmux attach -t default || tmux new -s default
+  tmux new -s "Dotfiles"
 fi
