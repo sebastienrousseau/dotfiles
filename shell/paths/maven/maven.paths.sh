4fc9eda09d98521320ee0792a96f47c30fb523d6
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-05 16:36:47 +0100

    feat(mvn): add MAVEN_OPTS

--- shell/paths/maven/maven.paths.sh
+++ shell/paths/maven/maven.paths.sh
@@ -6,11 +6,12 @@
 ## 🅼🅰🆅🅴🅽 🅷🅾🅼🅴
 if [[ -z "${MAVEN_HOME}" ]]; then
   if [[ "${OSTYPE}" == "darwin"* ]]; then
-    MAVEN_HOME="/opt/homebrew/Cellar/maven/"
+    MAVEN_HOME=/opt/homebrew/Cellar/maven/3.8.6/libexec
   elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
     MAVEN_HOME="/usr/share/maven/"
   fi
   M2_HOME="${MAVEN_HOME}"
   export MAVEN_HOME M2_HOME
   export PATH="${MAVEN_HOME}:${PATH}"
+  export MAVEN_OPTS="-Xms1g -Xmx1g"
 fi
