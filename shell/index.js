a8aefcc0edd0606131435db028bf237f4e494fe1
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-07 15:47:40 +0100

    fix(codacy):Strings must use doublequote.

--- shell/index.js
+++ shell/index.js
@@ -2,7 +2,7 @@
 const exec = require("child_process").exec;
 
 function main() {
-  exec('make installer', function (err, stdout) {
+  exec("make installer", function (err, stdout) {
     if (err) {
       console.error(err);
       return;
