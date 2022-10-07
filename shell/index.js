842a9520c76d3681d50f58554d0af45e23a7feae
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-07 21:00:56 +0100

    fix(js): removed unexpected console statement

--- shell/index.js
+++ shell/index.js
@@ -4,16 +4,12 @@ const exec = require("child_process").exec;
 function main() {
   exec("make installer", (error, stdout, stderr) => {
     if (error) {
-      console.error(`error: ${error.message}`);
       return;
     }
 
     if (stderr) {
-      console.error(`stderr: ${stderr}`);
       return;
     }
-
-    console.log(`stdout:\n${stdout}`);
   });
 }
 
