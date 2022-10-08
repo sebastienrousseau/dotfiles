0d5138d244874088fc7b53400a119b598cd25b0c
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-08 07:55:11 +0100

    feat(cli): added logic for LSCOLORS

--- shell/configurations/default/color.sh
+++ shell/configurations/default/color.sh
@@ -7,11 +7,16 @@
 export colorflag='-G' # Enable color output
 
 # Enable colored output from ls, etc. on FreeBSD-based systems
-unset LSCOLORS
+unset LSCOLORS LS_COLORS
 export CLICOLOR=1
 export CLICOLOR_FORCE=1
 
-export LSCOLORS='GxFxCxDxBxegedabagaced'
+# LSCOLORS
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+    export LSCOLORS="GxFxCxDxbxegedabagaced"
+else
+    export LS_COLORS="di=1;36:ln=1;35:so=1;32:pi=1;33:ex=1;31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=34;43"
+fi
 
 # Tell grep to highlight matches
 export GREP_OPTIONS='--color=auto'
