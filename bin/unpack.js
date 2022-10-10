858652043b5340ae5466f7e022cf14983d33d5c1
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-10 18:40:01 +0100

    Update bin/unpack.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- bin/unpack.js
+++ bin/unpack.js
@@ -9,7 +9,7 @@ async function unpack() {
   const compressing = require('compressing');
   var os = require('os');
   var path = require('path');
-  const { version } = require('./constants.js');
+  const { version } = require("./constants.js");
   var destPath = path.resolve(__dirname, os.homedir() + "/dotfiles_backup/");
   compressing.tgz.uncompress(destPath+"/"+version, destPath+"/");
 }
