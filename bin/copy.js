07d5d46b6578186e8564a1410295d29f05e0d0b2
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-10 18:40:21 +0100

    Update bin/copy.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- bin/copy.js
+++ bin/copy.js
@@ -8,7 +8,7 @@
 async function copy(src, dest) {
 
   var fs = require('fs');
-  var os = require('os');
+  var os = require("os");
   var path = require('path');
   var destPath = path.resolve(__dirname, os.homedir() + "/" + dest);
   var srcPath = path.resolve(__dirname, "../" + src);
