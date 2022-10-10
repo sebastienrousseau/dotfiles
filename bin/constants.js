8450078a73baf947ddd51362824ff430f18f5ea9
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-10 19:17:33 +0100

    0.2.458

--- bin/constants.js
+++ bin/constants.js
@@ -1,12 +1,12 @@
 /**
-* 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.457) - https://dotfiles.io
+* 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.458) - https://dotfiles.io
 * Copyright (c) Sebastien Rousseau 2022. All rights reserved
 * License: MIT
 */
 
 // 🅲🅾🅽🆂🆃🅰🅽🆃🆂 - Constants.
-const version = "dotfiles-0.2.457.tgz";
-const dotfile ="https://registry.npmjs.org/@sebastienrousseau/dotfiles/-/dotfiles-0.2.457.tgz";
+const version = "dotfiles-0.2.458.tgz";
+const dotfile ="https://registry.npmjs.org/@sebastienrousseau/dotfiles/-/dotfiles-0.2.458.tgz";
 
 const aliases = [
   ".bashrc",
