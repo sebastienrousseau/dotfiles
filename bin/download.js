642316d812ce725eb7f2d9c5ed88f76054f58a3b
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-10 18:42:37 +0100

    Update bin/download.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- bin/download.js
+++ bin/download.js
@@ -19,7 +19,7 @@ async function download() {
 
   const request = https.get(
     dotfile, response => {
-      console.log('STATUS: ' + response.statusCode);
+      console.log("STATUS: " + response.statusCode);
       var headers = JSON.stringify(response.headers);
       console.log('HEADERS: ' + headers);
       response.pipe(file);
