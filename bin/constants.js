86eff548633f94d2823fe77920fd64908b1c702d
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-12 21:58:04 +0100

    fix(codacy): function Call Object Injection Sink

--- bin/constants.js
+++ bin/constants.js
@@ -44,14 +44,6 @@ const config = [
   "lib/configurations/tmux/vi"
 ];
 
-const tmux  = [
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/",
-  "$DOTFILES/configurations/tmux/"
-];
+const tmux  = "$DOTFILES/configurations/tmux/";
 
 module.exports = { aliases, config, copies, dotfile, tmux, version };
