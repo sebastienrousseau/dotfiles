95ad667081702c25296ad95a880d3f38fd9bd2a1
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-10-16 00:04:23 +0100

    fix(scripts): updated compile.sh and scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,11 +6,46 @@
 ## 🅲🅾🅼🅿🅸🅻🅴 🅳🅾🆃🅵🅸🅻🅴🆂 - Compile dotfiles.
 
 compile() {
-  pnpm run cp:lib &&
-  pnpm run cl:tmp &&
-  pnpm run cp:bin &&
-  pnpm run cp:make &&
-  pnpm run minify &&
-  pnpm run filesizes
+
+  # shellcheck disable=SC1091
+  . "./lib/configurations/default/constants.sh"
+
+  # shellcheck disable=SC1091
+  . "./scripts/banner.sh"
+
+  echo ""
+  # shellcheck disable=SC2154
+  echo "${RED}❭${NC} Starting Compilation."
+  echo ""
+
+  echo "${GREEN}  ✔${NC} Copying libraries."
+  # shellcheck disable=SC1091
+  cp -R ./lib ./dist/
+
+  echo "${GREEN}  ✔${NC} Removing temporary files."
+  # shellcheck disable=SC1091
+  rimraf \"./dist/lib/**/*.tmp\"
+
+  echo "${GREEN}  ✔${NC} Copying JavaScript binaries."
+  cp -f -R ./bin ./dist/
+
+  echo "${GREEN}  ✔${NC} Copying Makefile."
+  cp -f ./Makefile ./dist
+
+  echo "${GREEN}  ✔${NC} Compressing JavaScript files."
+  jsmin ./bin/backup.js >dist/bin/backup.js && jsmin ./bin/constants.js >dist/bin/constants.js && jsmin ./bin/copy.js >dist/bin/copy.js && jsmin ./bin/dotfiles.js >dist/bin/dotfiles.js && jsmin ./bin/download.js >dist/bin/download.js && jsmin ./bin/index.js >dist/bin/index.js && jsmin ./bin/transfer.js >dist/bin/transfer.js && jsmin ./bin/unpack.js >dist/bin/unpack.js
+
+  echo "${GREEN}  ✔${NC} Determining the file sizes."
+  filesizes ./dist/ >./dist/filesizes.txt
+
+  # shellcheck disable=SC2154
+  echo "${GREEN}  ✔${NC} Compilation completed."
+  echo ""
 }
-compile
+
+args=$*               # Arguments passed to script.
+export args="${args}" # Exporting arguments.
+if [[ ${args} = "compile" ]]; then
+  echo "$*"
+  compile
+fi
