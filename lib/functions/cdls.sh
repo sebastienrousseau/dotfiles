72f43ba03f5107c3583d9395890cb8f332982ff3
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-11-05 16:09:48 +0000

    fix(functions): simplified `cdls` fn

--- lib/functions/cdls.sh
+++ lib/functions/cdls.sh
@@ -7,14 +7,7 @@
 # Function to combine cd and ls.
 # cdls: Function to combine cd and ls.
 cdls() {
-  # Add \ because cd loops with alias
-  if [[ "$#" != 1 ]]; then
-    echo "[ERROR] Please add one argument" >&2
-    return 1
-  fi
-  echo "[INFO] The operation completed successfully, directory listing of $1:"
-  \cd "$1" || exit
-  ls -lh
+  cd "$@" && ls
 }
 
 alias cdl='cdls' # alias for cdls
