27802da291bf826f4516ae111a6edd7872062b42
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-11-06 13:40:30 +0000

    fix(aliases): updated dig aliases

--- lib/aliases/dig/dig.aliases.sh
+++ lib/aliases/dig/dig.aliases.sh
@@ -6,11 +6,14 @@
 
 # 🅳🅸🅶 🅰🅻🅸🅰🆂🅴🆂
 if command -v dig &>/dev/null; then
+  alias d='$(which dig)'                                                            # d: Run the dig command with the default options.
+  alias d4='$(which dig) +short -4'                                                 # d4: Perform a DNS lookup for an IPv4 address.
+  alias d6='$(which dig) +short -6'                                                 # d6: Perform a DNS lookup for an IPv6 address.
   alias dga='$(which dig) +all ANY'                                                 # dga: Perform a DNS lookup for all records.
-  alias dgs='$(which dig) +short'                                                   # dgs: Perform a DNS lookup.
+  alias dgs='$(which dig) +short'                                                   # dgs: Perform a DNS lookup for a short answer.
   alias digg='$(which dig) @8.8.8.8 +nocmd any +multiline +noall +answer'           # digg: Dig with Google's DNS.
-  alias ip4='$(which dig) +short myip.opendns.com @resolver1.opendns.com -4'        # ip4: Display the public IPv4 address.
-  alias ip6='$(which dig) -6 AAAA +short myip.opendns.com. @resolver1.opendns.com.' # ip6: Get the public IPv6 address.
-  alias ips='ip4; ip6'                                                              # ips: Display all IP addresses.
-  alias wip='$(which dig) +short myip.opendns.com @resolver1.opendns.com'           # wip: Get public IP address.
+  alias ip4='$(which dig) +short myip.opendns.com @resolver1.opendns.com -4'        # ip4: Get your public IPv4 address.
+  alias ip6='$(which dig) -6 AAAA +short myip.opendns.com. @resolver1.opendns.com.' # ip6: Get your public IPv6 address.
+  alias ips='ip4; ip6'                                                              # ips: Get your public IPv4 and IPv6 addresses.
+  alias wip='$(which dig) +short myip.opendns.com @resolver1.opendns.com'           # wip: Get your public IP address.
 fi
