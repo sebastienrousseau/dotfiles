5d2c5863d649688f7607b1cbee798dc794e2c5cb
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-11-07 16:45:55 +0000

    fix(aliases): updated ps detection logic

--- lib/aliases/ps/ps.aliases.sh
+++ lib/aliases/ps/ps.aliases.sh
@@ -5,7 +5,7 @@
 # License: MIT
 
 # 🅿🆂 🅰🅻🅸🅰🆂🅴🆂 - PS aliases
-if command -v ps &>/dev/null; then
+if command -v 'ps' >/dev/null; then
   alias pid='ps -f'  # pid: Display the uid, pid, parent pid, recent CPU usage, process start time, controlling tty, elapsed CPU usage, and the associated command.
   alias ps='ps -ef'  # ps: Display all processes.
   alias psa='ps aux' # psa: List all processes.
