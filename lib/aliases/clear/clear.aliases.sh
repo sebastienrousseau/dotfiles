15d34719e0a2f3bb2488e12066ba25e9b16bb1d5
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-11-07 22:33:36 +0000

    fix(aliases): updated detection logic for clear

--- lib/aliases/clear/clear.aliases.sh
+++ lib/aliases/clear/clear.aliases.sh
@@ -5,19 +5,21 @@
 # License: MIT
 
 # 🅲🅻🅴🅰🆁 🅰🅻🅸🅰🆂🅴🆂
-alias c="clear"                                            # c: Clear the terminal screen.
-alias cl="c"                                               # cl: Clear the terminal screen.
-alias clc="clear && cat"                                   # clc: Clear the terminal screen and print the contents of the current directory.
-alias clcat="clc"                                          # clcat: Clear the terminal screen and print the contents of the current directory.
-alias clh="clear && history"                               # clh: Clear the terminal screen and print the command history.
-alias clhist="clh"                                         # clhist: Clear the terminal screen and print the command history.
-alias clp="clear && pwd"                                   # clp: Clear the terminal screen and print the current working directory.
-alias clpwd="clp"                                          # clpwd: Clear the terminal screen and print the current working directory.
-alias clr="c"                                              # clr: Clear the terminal screen.
-alias cls="c"                                              # cls: Clear the terminal screen.
-alias clt="clear && tree"                                  # clt: Clear the terminal screen and print the directory tree.
-alias cltree="clt"                                         # cltree: Clear the terminal screen and print the directory tree.
-alias ct="clear && tree ./"                                # ct: Clear the terminal screen and print the directory tree.
-alias ctree="ct"                                           # ctree: Clear the terminal screen and print the directory tree.
-alias cpl="clear && pwd && echo '' && ls -a && echo ''"    # cpl: Clear the terminal screen and print the current working directory and the contents of the current directory.
-alias cplt="clear && pwd && echo '' && tree ./ && echo ''" # cplt: Clear the terminal screen and print the current working directory and the directory tree.
+if command -v 'clear' >/dev/null; then
+  alias c="clear"                                            # c: Clear the terminal screen.
+  alias cl="c"                                               # cl: Clear the terminal screen.
+  alias clc="clear && cat"                                   # clc: Clear the terminal screen and print the contents of the current directory.
+  alias clcat="clc"                                          # clcat: Clear the terminal screen and print the contents of the current directory.
+  alias clh="clear && history"                               # clh: Clear the terminal screen and print the command history.
+  alias clhist="clh"                                         # clhist: Clear the terminal screen and print the command history.
+  alias clp="clear && pwd"                                   # clp: Clear the terminal screen and print the current working directory.
+  alias clpwd="clp"                                          # clpwd: Clear the terminal screen and print the current working directory.
+  alias clr="c"                                              # clr: Clear the terminal screen.
+  alias cls="c"                                              # cls: Clear the terminal screen.
+  alias clt="clear && tree"                                  # clt: Clear the terminal screen and print the directory tree.
+  alias cltree="clt"                                         # cltree: Clear the terminal screen and print the directory tree.
+  alias ct="clear && tree ./"                                # ct: Clear the terminal screen and print the directory tree.
+  alias ctree="ct"                                           # ctree: Clear the terminal screen and print the directory tree.
+  alias cpl="clear && pwd && echo '' && ls -a && echo ''"    # cpl: Clear the terminal screen and print the current working directory and the contents of the current directory.
+  alias cplt="clear && pwd && echo '' && tree ./ && echo ''" # cplt: Clear the terminal screen and print the current working directory and the directory tree.
+fi
