b6e1d8f3c5e5154e8128f290dfc43b6d9ca0f884
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-24 08:54:48 +0100

    feat(dotfiles): updated dig aliases

--- lib/aliases/dig/dig.aliases.sh
+++ lib/aliases/dig/dig.aliases.sh
@@ -6,14 +6,34 @@
 
 # 🅳🅸🅶 🅰🅻🅸🅰🆂🅴🆂
 if command -v dig &>/dev/null; then
-  alias d='$(which dig)'                                                            # d: Run the dig command with the default options.
-  alias d4='$(which dig) +short -4'                                                 # d4: Perform a DNS lookup for an IPv4 address.
-  alias d6='$(which dig) +short -6'                                                 # d6: Perform a DNS lookup for an IPv6 address.
-  alias dga='$(which dig) +all ANY'                                                 # dga: Perform a DNS lookup for all records.
-  alias dgs='$(which dig) +short'                                                   # dgs: Perform a DNS lookup for a short answer.
-  alias digg='$(which dig) @8.8.8.8 +nocmd any +multiline +noall +answer'           # digg: Dig with Google's DNS.
-  alias ip4='$(which dig) +short myip.opendns.com @resolver1.opendns.com -4'        # ip4: Get your public IPv4 address.
-  alias ip6='$(which dig) -6 AAAA +short myip.opendns.com. @resolver1.opendns.com.' # ip6: Get your public IPv6 address.
-  alias ips='ip4; ip6'                                                              # ips: Get your public IPv4 and IPv6 addresses.
-  alias wip='$(which dig) +short myip.opendns.com @resolver1.opendns.com'           # wip: Get your public IP address.
+  # d: Run the dig command with the default options.
+  alias d='$(which dig)'
+
+  # d4: Perform a DNS lookup for an IPv4 address.
+  alias d4='$(which dig) +short -4'
+
+  # d6: Perform a DNS lookup for an IPv6 address.
+  alias d6='$(which dig) +short -6'
+
+  # dga: Perform a DNS lookup for all records.
+  alias dga='$(which dig) +all ANY'
+
+  # dgs: Perform a DNS lookup for a short answer.
+  alias dgs='$(which dig) +short'
+
+  # digg: Dig with Google's DNS.
+  alias digg='$(which dig) @8.8.8.8 +nocmd any +multiline +noall +answer'
+
+  # ip4: Get your public IPv4 address.
+  alias ip4='$(which dig) +short myip.opendns.com @resolver1.opendns.com -4'
+
+  # ip6: Get your public IPv6 address.
+  alias ip6='$(which dig) -6 AAAA +short myip.opendns.com. @resolver1.opendns.com.'
+
+  # ips: Get your public IPv4 and IPv6 addresses.
+  alias ips='ip4; ip6'
+
+  # wip: Get your public IP address.
+  alias wip='$(which dig) +short myip.opendns.com @resolver1.opendns.com'
+
 fi
