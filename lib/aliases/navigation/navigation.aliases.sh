6e63ee86e5ab09adab239d51d4f92d556583a007
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-24 21:35:18 +0100

    fix(dotfiles): markdownlint_MD013

--- lib/aliases/navigation/navigation.aliases.sh
+++ lib/aliases/navigation/navigation.aliases.sh
@@ -6,19 +6,50 @@
 
 # 🅽🅰🆅🅸🅶🅰🆃🅸🅾🅽 🅰🅻🅸🅰🆂🅴🆂
 
-alias -- -='cd -'                       # -: Shortcut to go to previous directory.
-alias .....='cd ../../../..'            # .....: Shortcut to go to great-great-grandparent directory.
-alias ....='cd ../../..'                # ....: Shortcut to go to great-grandparent directory.
-alias ...='cd ../..'                    # ...: Shortcut to go to grandparent directory.
-alias ..='cd ..'                        # ..: Shortcut to go to parent directory.
-alias '~'='cd ~'                        # ~: Shortcut to go to home directory.
-alias app='cd ${HOME}/Applications; ls' # app: Shortcut to go to the Applications directory.
-alias cod='cd ${HOME}/Code; ls'         # cod: Shortcut to go to the Code directory and list its contents.
-alias des='cd ${HOME}/Desktop; ls'      # des Shortcut to go to the Desktop directory and list its contents.
-alias doc='cd ${HOME}/Documents; ls'    # doc: Shortcut to go to the Documents directory and list its contents.
-alias dot='cd ${HOME}/.dotfiles; ls'    # dot: Shortcut to go to the dotfiles directory.
-alias dow='cd ${HOME}/Downloads; ls'    # dow: Shortcut to go to the Downloads directory and list its contents.
-alias hom='cd ${HOME}/; ls'             # hom: Shortcut to go to home directory and list its contents.
-alias mus='cd ${HOME}/Music; ls'        # mus: Shortcut to go to the Music directory and list its contents.
-alias pic='cd ${HOME}/Pictures; ls'     # pic: Shortcut to go to the Pictures directory and list its contents.
-alias vid='cd ${HOME}/Videos; ls'       # vid: Shortcut to go to the Videos directory and list its contents.
+# Shortcut to go to previous directory.
+alias -- -='cd -'
+
+# Shortcut to go to great-great-grandparent directory.
+alias .....='cd ../../../..'
+
+# Shortcut to go to great-grandparent directory.
+alias ....='cd ../../..'
+
+# Shortcut to go to grandparent directory.
+alias ...='cd ../..'
+
+# Shortcut to go to parent directory.
+alias ..='cd ..'
+
+# Shortcut to go to home directory.
+alias '~'='cd ~'
+
+# Shortcut to go to the Applications directory.
+alias app='cd ${HOME}/Applications; ls'
+
+# Shortcut to go to the Code directory and list its contents.
+alias cod='cd ${HOME}/Code; ls'
+
+# Shortcut to go to the Desktop directory and list its contents.
+alias des='cd ${HOME}/Desktop; ls'
+
+# Shortcut to go to the Documents directory and list its contents.
+alias doc='cd ${HOME}/Documents; ls'
+
+# Shortcut to go to the dotfiles directory.
+alias dot='cd ${HOME}/.dotfiles; ls'
+
+# Shortcut to go to the Downloads directory and list its contents.
+alias dow='cd ${HOME}/Downloads; ls'
+
+# Shortcut to go to home directory and list its contents.
+alias hom='cd ${HOME}/; ls'
+
+# Shortcut to go to the Music directory and list its contents.
+alias mus='cd ${HOME}/Music; ls'
+
+# Shortcut to go to the Pictures directory and list its contents.
+alias pic='cd ${HOME}/Pictures; ls'
+
+# Shortcut to go to the Videos directory and list its contents.
+alias vid='cd ${HOME}/Videos; ls'
