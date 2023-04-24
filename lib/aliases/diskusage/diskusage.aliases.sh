d828dabb2d2c3d78ceee4e24cbf4931b69ff37f9
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-24 08:56:55 +0100

    feat(dotfiles): updated the disk usage aliases

--- lib/aliases/diskusage/diskusage.aliases.sh
+++ lib/aliases/diskusage/diskusage.aliases.sh
@@ -6,11 +6,25 @@
 
 # 🅳🅸🆂🅺 🆄🆂🅰🅶🅴 🅰🅻🅸🅰🆂🅴🆂
 if command -v du &>/dev/null; then
-  alias du="du -h"                                  # du: Display the disk usage of the current directory.
-  alias du1='du -hxd 1 | sort -h'                   # du1: File size of files and directories in current directory.
-  alias ducks="du -cks * .*| sort -rn | head -n 10" # ducks: Top 10 largest files and directories in current directory.
-  alias duh='du'                                    # duh: File size of files and directories.
-  alias dus='du -hs *'                              # dus: File size human readable output sorted by size.
-  alias dusym="du * -hsLc"                          # dusym: File size of files and directories in current directory including symlinks.
-  alias dut='dus'                                   # dut: Total file size of current directory.
+  # Display the disk usage of the current directory.
+  alias du="du -h"
+
+  # File size of files and directories in current directory.
+  alias du1='du -hxd 1 | sort -h'
+
+  # File size of files and directories.
+  alias duh='du'
+
+  # Top 10 largest files and directories in current directory.
+  alias ducks="du -cks * .*| sort -rn | head -n 10"
+
+  # File size human readable output sorted by size.
+  alias dus='du -hs *'
+
+  # File size of files and directories in current directory including symlinks.
+  alias dusym="du * -hsLc"
+
+  # Total file size of current directory.
+  alias dut='dus'
+
 fi
