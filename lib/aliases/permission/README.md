cc582b1d30c5b46a5be501d9fdcfd89544460878
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-24 22:28:07 +0100

    fix(dotfiles): markdownlint_MD013

---
+++ lib/aliases/permission/README.md
@@ -0,0 +1,32 @@
+# 🅿🅴🆁🅼🅸🆂🆂🅸🅾🅽 🅰🅻🅸🅰🆂🅴🆂
+
+<!-- markdownlint-disable MD033 MD041 -->
+
+<img src="https://kura.pro/dotfiles/v2/images/logos/dotfiles.svg"
+alt="dotfiles logo" width="261" align="right" />
+
+<!-- markdownlint-enable MD033 MD041 -->
+
+This code provides a set of aliases for file permissions.
+
+## Aliases
+
+- `000` (chmod a-rwx) sets permissions so that, (U)ser / owner can't read, can't write and can't execute. (G)roup can't read, can't write and can't execute. (O)thers can't read, can't write and can't execute.
+- `400` (chmod a-rw) sets permissions so that, (U)ser / owner can't read, can't write and can execute. (G)roup can't read, can't write and can execute. (O)thers can't read, can't write and can execute.
+- `444` (chmod a-r) sets permissions so that, (U)ser / owner can't read, can't write and can execute. (G)roup can't read, can't write and can execute. (O)thers can't read, can't write and can execute.
+- `600` (chmod a+rwx,u-x,g-rwx,o-rwx) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can't read, can't write and can't execute. (O)thers can't read, can't write and can't execute.
+- `644` (chmod a+rwx,u-x,g-wx,o-wx) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can read, can't write and can't execute. (O)thers can read, can't write and can't execute.
+- `666` (chmod a+rwx,u-x,g-x,o-x) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can read, can write and can't execute. (O)thers can read, can write and can't execute.
+- `755` (chmod a+rwx,g-w,o-w) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can't write and can execute. (O)thers can read, can't write and can execute.
+- `764` (chmod a+rwx,g-x,o-wx) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can write and can't execute. (O)thers can read, can't write and can't execute.
+- `777` (chmod a+rwx) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can write and can execute. (O)thers can read, can write and can execute.
+- `chgrp` Change group ownership of files or directories.
+- `chgrpr` Change group ownership of files or directories recursively.
+- `chgrpu` Change group ownership of files or directories recursively to the current user.
+- `chmod` Change file mode bits.
+- `chmodr` Change file mode bits recursively.
+- `chmodu` Change file mode bits recursively to the current user.
+- `chmox` Make a file executable.
+- `chown` Change file owner and group.
+- `chownr` Change file owner and group recursively.
+- `chownu` Change file owner and group recursively to the current user.
--- lib/aliases/permission/permission.aliases.sh
+++ lib/aliases/permission/permission.aliases.sh
@@ -5,24 +5,84 @@
 # License: MIT
 
 # 🅿🅴🆁🅼🅸🆂🆂🅸🅾🅽 🅰🅻🅸🅰🆂🅴🆂
+
 if command -v chmod &>/dev/null; then
-  alias 000='chmod -R 000'         # 000: (chmod a-rwx) sets permissions so that, (U)ser / owner can't read, can't write and can't execute. (G)roup can't read, can't write and can't execute. (O)thers can't read, can't write and can't execute.
-  alias 400='chmod -R 400'         # 400: (chmod a-rw) sets permissions so that, (U)ser / owner can't read, can't write and can execute. (G)roup can't read, can't write and can execute. (O)thers can't read, can't write and can execute.
-  alias 444='chmod -R 444'         # 444: (chmod a-r) sets permissions so that, (U)ser / owner can't read, can't write and can execute. (G)roup can't read, can't write and can execute. (O)thers can't read, can't write and can execute.
-  alias 600='chmod -R 600'         # 600: (chmod a+rwx,u-x,g-rwx,o-rwx) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can't read, can't write and can't execute. (O)thers can't read, can't write and can't execute.
-  alias 644='chmod -R 644'         # 644: (chmod a+rwx,u-x,g-wx,o-wx) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can read, can't write and can't execute. (O)thers can read, can't write and can't execute.
-  alias 666='chmod -R 666'         # 666: (chmod a+rwx,u-x,g-x,o-x) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can read, can write and can't execute. (O)thers can read, can write and can't execute.
-  alias 755='chmod -R 755'         # 755: (chmod a+rwx,g-w,o-w) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can't write and can execute. (O)thers can read, can't write and can execute.
-  alias 764='chmod -R 764'         # 764: (chmod a+rwx,g-x,o-wx) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can write and can't execute. (O)thers can read, can't write and can't execute.
-  alias 777='chmod -R 777'         # 777: (chmod a+rwx) sets permissions so that, (U)ser / owner can read, can write and can execute. (G)roup can read, can write and can execute. (O)thers can read, can write and can execute.
-  alias chgrp='chgrp -v'           # chgrp: Change group ownership of files or directories.
-  alias chgrpr='chgrp -Rv'         # chgrpr: Change group ownership of files or directories recursively.
-  alias chgrpu='chgrp -Rv ${USER}' # chgrpu: Change group ownership of files or directories recursively to the current user.
-  alias chmod='chmod -v'           # chmod: Change file mode bits.
-  alias chmodr='chmod -Rv'         # chmodr: Change file mode bits recursively.
-  alias chmodu='chmod -Rv u+rwX'   # chmodu: Change file mode bits recursively to the current user.
-  alias chmox='chmod +x'           # chmox: Make a file executable.
-  alias chown='chown -v'           # chown: Change file owner and group.
-  alias chownr='chown -Rv'         # chownr: Change file owner and group recursively.
-  alias chownu='chown -Rv ${USER}' # chownu: Change file owner and group recursively to the current user.
+
+  # (chmod a-rwx) sets permissions so that, (U)ser / owner can't read,
+  # can't write and can't execute. (G)roup can't read, can't write and
+  # can't execute. (O)thers can't read, can't write and can't execute.
+  alias 000='chmod -R 000'
+
+  # (chmod a-rw) sets permissions so that, (U)ser / owner can't read,
+  # can't write and can execute. (G)roup can't read, can't write and can
+  # execute. (O)thers can't read, can't write and can execute.
+  alias 400='chmod -R 400'
+
+  # (chmod a-r) sets permissions so that, (U)ser / owner can't read,
+  # can't write and can execute. (G)roup can't read, can't write and can
+  # execute. (O)thers can't read, can't write and can execute.
+  alias 444='chmod -R 444'
+
+  # (chmod a+rwx,u-x,g-rwx,o-rwx) sets permissions so that, (U)ser /
+  # owner can read, can write and can't execute. (G)roup can't read,
+  # can't write and can't execute. (O)thers can't read, can't write and
+  # can't execute.
+  alias 600='chmod -R 600'
+
+  # (chmod a+rwx,u-x,g-wx,o-wx) sets permissions so that, (U)ser /
+  # owner can read, can write and can't execute. (G)roup can read,
+  # can't write and can't execute. (O)thers can read, can't write and
+  # can't execute.
+  alias 644='chmod -R 644'
+
+  # (chmod a+rwx,u-x,g-x,o-x) sets permissions so that, (U)ser / owner
+  # can read, can write and can't execute. (G)roup can read, can write
+  # and can't execute. (O)thers can read, can write and can't execute.
+  alias 666='chmod -R 666'
+
+  # (chmod a+rwx,g-w,o-w) sets permissions so that, (U)ser / owner can
+  # read, can write and can execute. (G)roup can read, can't write and
+  # can execute. (O)thers can read, can't write and can execute.
+  alias 755='chmod -R 755'
+
+  # (chmod a+rwx,g-x,o-wx) sets permissions so that, (U)ser / owner
+  # can read, can write and can execute. (G)roup can read, can write and
+  # can't execute. (O)thers can read, can't write and can't execute.
+  alias 764='chmod -R 764'
+
+  # (chmod a+rwx) sets permissions so that, (U)ser / owner can read,
+  # can write and can execute. (G)roup can read, can write and can
+  # execute. (O)thers can read, can write and can execute.
+  alias 777='chmod -R 777'
+
+  # Change group ownership of files or directories.
+  alias chgrp='chgrp -v'
+
+  # Change group ownership of files or directories recursively.
+  alias chgrpr='chgrp -Rv'
+
+  # Change group ownership of files or directories recursively to the
+  # current user.
+  alias chgrpu='chgrp -Rv ${USER}'
+
+  # Change file mode bits.
+  alias chmod='chmod -v'
+
+  # Change file mode bits recursively.
+  alias chmodr='chmod -Rv'
+
+  # Change file mode bits recursively to the current user.
+  alias chmodu='chmod -Rv u+rwX'
+
+  # Make a file executable.
+  alias chmox='chmod +x'
+
+  # Change file owner and group.
+  alias chown='chown -v'
+
+  # Change file owner and group recursively.
+  alias chownr='chown -Rv'
+
+  # Change file owner and group recursively to the current user.
+  alias chownu='chown -Rv ${USER}'
 fi
