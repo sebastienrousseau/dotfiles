206d6e2fffa3eacc1dcee6450a170173878c01c1
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-26 09:05:32 +0100

    feat(dotfiles): updated wget aliases

---
+++ lib/aliases/wget/README.md
@@ -0,0 +1,17 @@
+# 🆆🅶🅴🆃 🅰🅻🅸🅰🆂🅴🆂
+
+<!-- markdownlint-disable MD033 MD041 -->
+
+<img src="https://kura.pro/dotfiles/v2/images/logos/dotfiles.svg"
+alt="dotfiles logo" width="261" align="right" />
+
+<!-- markdownlint-enable MD033 MD041 -->
+
+This code provides a set of aliases for the `wget` command.
+
+## 🅰🅻🅸🅰🆂🅴🆂
+
+- `wg` wget.
+- `wgc` wget with continue.
+- `wge` wget with robots=off.
+- `wget` wget with continue.
--- lib/aliases/wget/wget.aliases.sh
+++ lib/aliases/wget/wget.aliases.sh
@@ -7,8 +7,16 @@
 
 # 🆆🅶🅴🆃 🅰🅻🅸🅰🆂🅴🆂
 if command -v 'wget' >/dev/null; then
-  alias wg='wget'              # wg: wget.
-  alias wgc='wg'               # wgc: wget with continue.
-  alias wge='wg -e robots=off' # wge: wget with robots=off.
-  alias wget='wget -c'         # wget: wget with continue.
+
+  # wget.
+  alias wg='wget'
+
+  # wget with continue.
+  alias wgc='wg'
+
+  # wget with robots=off.
+  alias wge='wg -e robots=off'
+
+  # wget with continue.
+  alias wget='wget -c'
 fi
