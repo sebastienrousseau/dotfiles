f304d6183f6f941962ab0c1f3dff7b8edf0e62fd
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-26 09:55:36 +0100

    chore(dotfiles): cleaning up uuid

--- lib/aliases/uuid/uuid.aliases.sh
+++ lib/aliases/uuid/uuid.aliases.sh
@@ -9,7 +9,6 @@
 
 # uuid: Generate a UUID and copy it to the clipboard.
 if [[ "${OSTYPE}" == "darwin"* ]]; then
-
   if command -v 'uuidgen' >/dev/null; then
     # macOS
     alias uuid="uuidgen | tr -d '\n' | tr '[:upper:]' '[:lower:]' | pbcopy && pbpaste && echo"
