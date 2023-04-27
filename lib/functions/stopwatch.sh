3dd83bd84769e34ac4dc3a8d0a16998626618e7c
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-27 08:29:09 +0100

    fix(dotfiles): minor tweaks

--- lib/functions/stopwatch.sh
+++ lib/functions/stopwatch.sh
@@ -9,7 +9,7 @@
 stopwatch() {
   date1=$(gdate +%s)
   while true; do
-    echo -ne "$(gdate -u --date @$(($(date +%s) - date1)) +%H:%M:%S)\r"
+    echo -ne "$(gdate -u --date @$(($(date +%s) - date1)) +%H:%M:%S || true)\r"
     sleep 0.1
   done
 }
