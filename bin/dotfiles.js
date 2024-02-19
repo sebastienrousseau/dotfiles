af2752043506e143e3a01864b4205c97ffc64e92
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2024-02-19 17:53:18 +0000

    fix(dotfiles): :bug: Async function 'main' has a complexity of 5

--- bin/dotfiles.js
+++ bin/dotfiles.js
@@ -1,47 +1,58 @@
-// 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.468) - <https://dotfiles.io>
-// Made with ♥ in London, UK by @wwdseb
-// Copyright (c) 2015-2024. All rights reserved
-// License: MIT
+/**
+* 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.468) - <https://dotfiles.io>
+* Made with ♥ in London, UK by @wwdseb
+* Copyright (c) 2015-2024. All rights reserved
+* License: MIT
+*/
 
-// 🅼🅰🅸🅽 - Main function.
-const copy = require("./copy.js");
-const transfer = require("./transfer.js");
-const backup = require("./backup.js");
-const download = require("./download.js");
-const unpack = require("./unpack.js");
-const { aliases, copies } = require("./constants.js");
-const sleep = (waitTimeInMs) => new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
-const fs = require("fs").promises; // Import promises version of fs
-const os = require("os");
-const path = require("path");
+import backup from "./backup.js";
+import copy from "./copy.js";
+import download from "./download.js";
+import transfer from "./transfer.js";
+import unpack from "./unpack.js";
+import { aliases, copies } from "./constants.js";
+import fs from "fs/promises";
+import os from "os";
+import path from "path";
 
 const dir = path.resolve(__dirname, os.homedir());
 
-module.exports = async function main() {
+const sleep = (waitTimeInMs) => new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
 
-  let i = 0;
+const main = async () => {
+  await backupAndCopy();
+  await downloadAndUnpack();
+  await createDirIfNeeded();
+  await transferFiles();
+};
 
-  // Backup files and copy dotfiles.
-  do {
+const backupAndCopy = async () => {
+  for (let i = 0; i < Math.min(aliases.length, copies.length); i++) {
     await backup(aliases[i], aliases[i]);
     await copy(copies[i], aliases[i]);
-    i++;
-  } while (i < aliases.length && i < copies.length);
+  }
+};
 
-  // Download and unpack dotfiles.
-  download; // download the dotfiles
+const downloadAndUnpack = async () => {
+  download(); // download the dotfiles
   await sleep(2500); // wait for download to complete
   unpack(); // unpack the downloaded file
   await sleep(2500); // wait for unpack to complete
+};
 
+const createDirIfNeeded = async () => {
   try {
     await fs.access(dir); // Check if directory exists asynchronously
   } catch (error) {
-    if (error.code === 'ENOENT') { // If directory does not exist
+    if (error.code === "ENOENT") { // If directory does not exist
       await fs.mkdir(dir); // Create directory asynchronously
       await sleep(2500); // wait for mkdir to complete
     }
   }
+};
 
+const transferFiles = async () => {
   await transfer(dir);
 };
+
+export default main;
