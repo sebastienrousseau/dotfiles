db5f6bd4fe1b0fbe4d85f11a38eb3bbb5d9cf068
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2024-02-19 17:38:12 +0000

    fix(dotfiles): :bug: use async file system methods and handle directory creation asynchronously

--- bin/dotfiles.js
+++ bin/dotfiles.js
@@ -1,9 +1,7 @@
-/**
-* 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.468) - <https://dotfiles.io>
-* Made with ♥ in London, UK by @wwdseb
-* Copyright (c) 2015-2024. All rights reserved
-* License: MIT
-*/
+// 🅳🅾🆃🅵🅸🅻🅴🆂 (v0.2.468) - <https://dotfiles.io>
+// Made with ♥ in London, UK by @wwdseb
+// Copyright (c) 2015-2024. All rights reserved
+// License: MIT
 
 // 🅼🅰🅸🅽 - Main function.
 const copy = require("./copy.js");
@@ -13,9 +11,9 @@ const download = require("./download.js");
 const unpack = require("./unpack.js");
 const { aliases, copies } = require("./constants.js");
 const sleep = (waitTimeInMs) => new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
-var fs = require("fs");
-var os = require("os");
-var path = require("path");
+const fs = require("fs").promises; // Import promises version of fs
+const os = require("os");
+const path = require("path");
 
 const dir = path.resolve(__dirname, os.homedir());
 
@@ -25,8 +23,8 @@ module.exports = async function main() {
 
   // Backup files and copy dotfiles.
   do {
-    backup(aliases[i], aliases[i]);
-    copy(copies[i], aliases[i]);
+    await backup(aliases[i], aliases[i]);
+    await copy(copies[i], aliases[i]);
     i++;
   } while (i < aliases.length && i < copies.length);
 
@@ -35,10 +33,15 @@ module.exports = async function main() {
   await sleep(2500); // wait for download to complete
   unpack(); // unpack the downloaded file
   await sleep(2500); // wait for unpack to complete
-  if (!fs.existsSync(dir)) {
-    // Use fs.promises.mkdir for async operation
-    await fs.promises.mkdir(dir);
-    await sleep(2500); // wait for mkdir to complete
+
+  try {
+    await fs.access(dir); // Check if directory exists asynchronously
+  } catch (error) {
+    if (error.code === 'ENOENT') { // If directory does not exist
+      await fs.mkdir(dir); // Create directory asynchronously
+      await sleep(2500); // wait for mkdir to complete
+    }
   }
-  await transfer(dir); // transfer the unpacked files to the home directory
+
+  await transfer(dir);
 };
