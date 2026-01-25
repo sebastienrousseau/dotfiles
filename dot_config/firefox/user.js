bfd977a1252438fdd8ac7599be965b285c720ff0
Sebastien Rousseau <sebastian.rousseau@gmail.com>
2026-01-25 09:00:47 +0000

    CI: silence Codacy and disable unauth cache

--- dot_config/firefox/user.js
+++ dot_config/firefox/user.js
@@ -1,4 +1,5 @@
 // Minimal Firefox user.js defaults (review before use).
+/* global user_pref */
 user_pref("browser.startup.page", 3);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.search.suggest.enabled", false);
--- templates/projects/node/src/index.js
+++ templates/projects/node/src/index.js
@@ -1,1 +1,2 @@
+// eslint-disable-next-line no-console
 console.log("__PROJECT_NAME__");
